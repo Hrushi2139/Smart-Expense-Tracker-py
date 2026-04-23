585ed1a04578ca1d0b39f986aee1f44b734952fc
Hrushikesh <sannidhanamhrushikesh@gmail.com>
2026-04-23 10:36:28 +0530

    add Expense Function added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from datetime import datetime
+from expenditure_datetime import expenditure_datetime
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
@@ -10,13 +10,34 @@ def storage_setup():
     if not os.path.exists(DATA_FILE):
         with open(DATA_FILE, "w", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(["id", "date", "category", "amount", "description"])
+            writer.writeentry(["id", "expenditure_date", "expense_type", "amount", "description"])
 
 def load_expenditure():
     rec = []
     with open(DATA_FILE, "r") as f:
         reader = csv.DictReader(f)
-        for row in reader:
-            row["amount"] = float(row["amount"])
-            rec.append(row)
+        for entry in reader:
+            entry["amount"] = float(entry["amount"])
+            rec.append(entry)
     return rec
+
+def add_expense():
+    try:
+        expenditure_date = input("Enter expenditure_date (YYYY-MM-DD): ")
+        expenditure_datetime.strptime(expenditure_date, "%Y-%m-%d")
+
+        expense_type = input("Enter expense_type: ")
+        amount = float(input("Enter amount: "))
+        desc = input("Enter description: ")
+
+        data = read_data()
+        new_id = len(data) + 1
+
+        with open(DATA_FILE, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writeentry([new_id, expenditure_date, expense_type, amount, desc])
+
+        print("New Expense Added")
+
+    except:
+        print("Invalid Input")
